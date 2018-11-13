72ef261d5faac18bc07b0e1d9c052250ffa51e5a
aserfend <liuchangm7j@vip.qq.com>
2018-11-13 15:46:59 +0800

    null

---
+++ 算法训练/k好数.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+#define min(a,b) a<b?a:b  
+int main(){  
+int i,j,k,e;  
+long long K,L,q=0;  
+long long a[1500][2]={0};  
+scanf("%lld",&k);  
+
+printf("%ld",k);  
+return 0;  
+}  
